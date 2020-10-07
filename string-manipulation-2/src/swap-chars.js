c8d274a9993bcaae96125df33aa606a8bdba55be
Joanna Pham <jopham90@gmail.com>
2020-10-07 10:11:24 -0700

    work in progress

--- string-manipulation-2/src/swap-chars.js
+++ string-manipulation-2/src/swap-chars.js
@@ -3,6 +3,6 @@
 // take an index in string
 // location new location named secondIndex
 
-// function swapChars(firstIndex, secondIndex, string) {
-
-// }
+function swapChars(firstIndex, secondIndex, string) {
+  return string.substr(0, firstIndex) + string[secondIndex];
+}
