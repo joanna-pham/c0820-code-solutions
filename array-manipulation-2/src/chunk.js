fe058f0518131df0a4b8d5cca4b63f1969ef30b8
Joanna Pham <jopham90@gmail.com>
2020-10-11 14:17:36 -0700

    completed exercise

--- array-manipulation-2/src/chunk.js
+++ array-manipulation-2/src/chunk.js
@@ -1,3 +1,19 @@
 /* eslint-disable no-unused-vars */
 
-// try twice one with slice and another without slice
+function chunk(array, size) {
+  const result = [];
+  let tempResult = [];
+  for (let i = 0; i < array.length; i++) {
+    if (tempResult.length <= size) { // how many items are in array
+      tempResult.push(array[i]);
+    }
+    if (tempResult.length === size) {
+      result.push(tempResult);
+      tempResult = [];
+    }
+    if (array.length - 1 === i && tempResult.length > 0) {
+      result.push(tempResult);
+    }
+  }
+  return result;
+}
--- array-manipulation-2/src/drop-right.js
+++ array-manipulation-2/src/drop-right.js
@@ -3,7 +3,10 @@
 function dropRight(array, count) {
   const result = [];
 
-  for (let i = 0; i < array.length; i++) {
+  if (count > array.length) {
+    return result;
+  }
+  for (let i = 0; i < array.length - count; i++) {
     result.push(array[i]);
   }
   return result;
