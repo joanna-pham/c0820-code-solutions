d07ada20929131e5c3396effce3e3cddcd3147e7
Joanna Pham <jopham90@gmail.com>
2020-10-15 11:53:19 -0700

    saving progress

--- object-manipulation-2/src/omit.js
+++ object-manipulation-2/src/omit.js
@@ -1,1 +1,15 @@
 /* eslint-disable no-unused-vars */
+
+function omit(source, keys) {
+  const result = {};
+  const tempResult = {};
+  for (let i = 0; i < keys.length; i++) {
+    // console.log('source', source);
+    console.log('keys[i]', keys[i]);
+    console.log('source[keys[i]]', source[keys[i]]);
+    if (keys[i] !== source[keys[i]]) {
+      result[keys[i]] = source[keys[i]];
+    }
+    return result;
+  }
+}
--- object-manipulation-2/src/pick.js
+++ object-manipulation-2/src/pick.js
@@ -1,17 +1,16 @@
 /* eslint-disable no-unused-vars */
 
-function pick(source, keys) {
-  const result = {};
-  for (let i = 0; i < keys.length; i++) {
-    if (source[keys[i]]) {
-      console.log('source[keys[i]]', source[keys[i]]);
-      console.log('keys[i]', keys[i]);
-      result[keys[i]] = source[keys[i]];
-      // console.log
-    }
-  }
-  return result;
-}
+// function pick(source, keys) {
+//   const result = {};
+//   for (let i = 0; i < keys.length; i++) {
+//     if (keys[i] === source[keys]) {
+//       // console.log('source[keys[i]]', source[keys[i]]);
+//       // console.log('keys[i]', keys[i]);
+//       result[keys[i]] = source[keys[i]];
+//     }
+//   }
+//   return result;
+// }
 
 // prop --> key
 // source[prop] --> value
