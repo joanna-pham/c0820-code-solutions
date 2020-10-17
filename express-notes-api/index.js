1c99adb097be5ede0d21722e50894076cc0915e1
Joanna Pham <jopham90@gmail.com>
2020-10-17 16:04:23 -0700

    saving progress

--- express-notes-api/index.js
+++ express-notes-api/index.js
@@ -1,9 +1,8 @@
 const express = require('express');
 const app = express();
-
 const data = require('./data.json');
-
 const fs = require('fs');
+const { response } = require('express');
 
 const arrayJson = [];
 
@@ -30,8 +29,6 @@ app.get('/api/notes/:id', (req, res, next) => {
 });
 
 app.post('/api/notes', (req, res, next) => {
-  // has a content
-  // no content - 400
   if (!req.body.content) {
     res.status(400).json({ error: 'content is a required field' });
     return;
@@ -42,22 +39,12 @@ app.post('/api/notes', (req, res, next) => {
     id: dataJson.nextId,
     content: req.body.content
   };
+
   let nextId = dataJson.nextId;
-
   dataJson.notes[nextId] = newData;
-
   nextId++;
   dataJson.nextId = nextId;
   dataJson = JSON.stringify(dataJson, null, 2);
-  console.log('dataJson', dataJson);
-  // console.log('newData', newData);
-  // send a response with an id and content
-  // content is easy
-  // how to send id
-  // update the data.json
-
-  // use fs module
-  // need to use writefile
 
   fs.writeFile('./data.json', dataJson, err => {
     if (err) {
@@ -66,16 +53,51 @@ app.post('/api/notes', (req, res, next) => {
       res.status(201).json(newData);
     }
   });
+});
 
-  // get the data.json
-  // create a new object {id: , content: }
-  // find the id
-  // incretment id
-  // update data.json
-  // stringinfy new object
-  // try to write to data.json
-  // if it doesn't write : send 500
-  // if it does write send 201
+app.delete('/api/notes/:id', (req, res, next) => {
+  let dataJson = data;
+
+  const idNum = parseInt(req.params.id, 10);
+  if (idNum < 1 || !idNum) {
+    res.status(400).json({ error: 'id must be a positive integer' });
+  } else if (data.notes[idNum]) {
+    delete data.notes[idNum];
+    dataJson = JSON.stringify(dataJson, null, 2);
+    fs.writeFile('./data.json', dataJson, err => {
+      if (err) {
+        res.status(500).json({ error: 'An unexpected error occurred.' });
+      }
+      res.sendStatus(204);
+    });
+  } else {
+    res.status(404).json({ error: 'cannot find note with id ' + idNum });
+  }
+});
+
+app.put('/api/notes/:id', (req, res, next) => {
+  // no valid id (positive int) or no content
+  // --400 with error property
+  // valid id and content but no matching note
+  // --404 response
+  // valid id and content but error on file
+  // --500 response
+  // valid id and content and updated successfully
+  // --200 response
+  const dataJson = data;
+
+  const idNum = parseInt(req.params.id, 10);
+
+  // if (idNum === dataJson.notes[idNum].id && typeof req.body.content !== 'undefined') {
+  //   res.status(200).json(dataJson.notes[idNum]);
+  // } else {
+  //   res.status(400).json({ error: 'content is a required field' });
+  // }
+
+  if (!idNum || idNum < 1) {
+    console.log('running this');
+    res.status(400).json({ error: 'id must be a positive integer' });
+  }
 
 });
 
