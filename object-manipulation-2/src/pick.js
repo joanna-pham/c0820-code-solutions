419612527222bf38cc3177587e6e33564bc25607
Joanna Pham <jopham90@gmail.com>
2020-10-17 18:25:09 -0700

    saving progress

--- object-manipulation-2/src/pick.js
+++ object-manipulation-2/src/pick.js
@@ -1,16 +1,12 @@
 /* eslint-disable no-unused-vars */
 
-// function pick(source, keys) {
-//   const result = {};
-//   for (let i = 0; i < keys.length; i++) {
-//     if (keys[i] === source[keys]) {
-//       // console.log('source[keys[i]]', source[keys[i]]);
-//       // console.log('keys[i]', keys[i]);
-//       result[keys[i]] = source[keys[i]];
-//     }
-//   }
-//   return result;
-// }
-
-// prop --> key
-// source[prop] --> value
+function pick(source, keys) {
+  const result = {};
+  for (let i = 0; i < keys.length; i++) {
+    const prop = keys[i];
+    if (typeof source[prop] !== 'undefined') {
+      result[prop] = source[prop];
+    }
+  }
+  return result;
+}
