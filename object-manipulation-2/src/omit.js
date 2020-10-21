ddea9dc6f03808276e23dbf80346986527a36435
Joanna Pham <jopham90@gmail.com>
2020-10-20 17:01:45 -0700

    saving progress

--- object-manipulation-2/src/omit.js
+++ object-manipulation-2/src/omit.js
@@ -1,18 +1,18 @@
 /* eslint-disable no-unused-vars */
 
 // loop though keys
-// if prop does not exist in keys, push prop to result
+// if prop does not exist in keys, push source prop to result
 
-// function omit(source, keys) {
-//   const result = {};
-//   for (let i = 0; i < keys.length; i++) {
-//     const prop = keys[i];
-//     console.log('prop', prop);
-//     console.log('source', source);
-//     console.log('source[prop]', source[prop]);
-//     if (source[prop] !== prop) {
-
-//     }
-//   }
-//   return result;
-// }
+function omit(source, keys) {
+  const result = {};
+  for (let i = 0; i < keys.length; i++) {
+    const prop = keys[i];
+    console.log('prop', prop);
+    console.log('source', source);
+    console.log('source[prop]', source[prop]);
+    if (typeof source[prop] === 'undefined') {
+      result.push(source[prop]);
+    }
+  }
+  return result;
+}
