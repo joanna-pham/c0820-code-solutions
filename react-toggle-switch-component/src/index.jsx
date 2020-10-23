519f8235e61a226a6b5a741d292f3d1daa69c12c
Joanna Pham <jopham90@gmail.com>
2020-10-22 18:02:06 -0700

    completed react toggle switch component exercise

---
+++ react-toggle-switch-component/src/index.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import ToggleSwitch from './toggle-switch';
+
+ReactDOM.render(
+  <ToggleSwitch />,
+  document.querySelector('#root')
+);
+
+export default ToggleSwitch;
