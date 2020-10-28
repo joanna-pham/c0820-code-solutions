44e1b39bbe3ce47f3a5ec9c933592354b4ef6351
Joanna Pham <jopham90@gmail.com>
2020-10-28 12:45:20 -0700

    completed Object Manipulation 2

--- object-manipulation-2/src/defaults.js
+++ object-manipulation-2/src/defaults.js
@@ -1,11 +1,11 @@
 /* eslint-disable no-unused-vars */
 
 function defaults(target, source) {
+  // console.log('target', target);
+  // console.log('source', source);
   for (const prop in source) {
-    console.log('prop', prop);
-    console.log('source[prop]', source[prop]);
-    if (//check undefined){
-
+    if (typeof target[prop] === 'undefined') {
+      target[prop] = source[prop];
     }
   }
 }
